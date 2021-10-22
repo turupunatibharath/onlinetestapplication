2ded03b3414e86cb71e2d8e48f2301223c87663a
turupunatibharath <bharathturupunati01@gmail.com>
2021-10-22 17:13:21 +0530

    first commit

---
+++ js/quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"On which platform java runs",
+        option:[
+            "1.Windows",
+            "2.Mac Os",
+            "3.UNIX",
+            "4.All of these",
+        ],
+        answer:4,
+    },
+    {
+        question:"Angular is based on",
+        option:[
+            "1.MVC A rchitecture",
+            "2. MVVM Architectural pattern",
+            "3. Decorator pattern",
+            "4. Observer Pattern",
+            
+        ],
+        answer:2,
+    },
+    {
+        question:"Which type of JavaScript language is?",
+        option:[
+            "1.Object-Oriented",
+            "2.Object-Based",
+            "3.Assembly-language",
+           "4.High-level",   
+        ],
+        answer:2,
+    },
+    {
+        question:"How to create a checkbox in HTML?",
+        option:[
+            '1.<input type = "checkbox">',
+            '2.<input type = "button">',
+            '3.<checkbox>',
+            '4.<input type = "check">',   
+        ],
+        answer:1,
+
+    },
+    {
+        question:"The function and var are known as",
+        option:[
+            "1.Keywords",
+            "2.Data types",
+            "3.Declaration statements",
+            "4.Prototypes",   
+        ],
+        answer:3,
+    }
+]
